02ceeb9460fbb1676ea409686e53d1a02a0ca122
yigityalin <yigityalin02@gmail.com>
2021-04-29 15:41:47 +0300

    image buttons fixed

--- app/src/main/java/com/g2k/calendy/CalendarAdapter.java
+++ app/src/main/java/com/g2k/calendy/CalendarAdapter.java
@@ -3,6 +3,7 @@ package com.g2k.calendy;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
