ded5765fd9acd58f8c95440bae5f6928464dc59f
cmglmsr <mgulumser@my.aci.k12.tr>
2021-04-30 23:07:30 +0300

    welcome page renamed to start activity

--- app/src/main/java/com/g2k/calendy/StartActivity.java
+++ app/src/main/java/com/g2k/calendy/StartActivity.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class WelcomePageActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome_page);
+        setContentView(R.layout.activity_start);
     }
 
     public void logInAction(View view) {
